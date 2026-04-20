10377ec0fe2ea35da22f903f7560ce4c265aa9b4
szbgreg <74787170+szbgreg@users.noreply.github.com>
2026-04-20 20:32:29 +0200

    Navigation drawer és alap navigáció bekötése

--- app/src/main/java/hu/nje/recipefinder/MainActivity.java
+++ app/src/main/java/hu/nje/recipefinder/MainActivity.java
@@ -4,8 +4,18 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +27,31 @@ public class MainActivity extends AppCompatActivity {
 
         setTitle("RecipeFinder");
 
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.nav_host_fragment);
+
+        navController = navHostFragment.getNavController();
+
+        appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.homeFragment,
+                R.id.favoritesFragment,
+                R.id.shoppingListFragment
+        ).setOpenableLayout(drawerLayout).build();
+
+        NavigationUI.setupActionBarWithNavController(
+                this, navController, appBarConfiguration
+        );
+
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
