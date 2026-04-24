82eaf2d95b9faf1e45bb4af4622f87d5d90e485b
MadaraszMark <madaraszmark461@gmail.com>
2026-04-24 12:26:14 +0200

    DTO modellek létrehozása API-hoz

---
+++ app/src/main/java/hu/nje/recipefinder/data/remote/dto/MealDto.java
@@ -0,0 +1,35 @@
+package hu.nje.recipefinder.data.remote.dto;
+
+public class MealDto {
+
+    private String idMeal;
+    private String strMeal;
+    private String strCategory;
+    private String strArea;
+    private String strInstructions;
+    private String strMealThumb;
+
+    public String getIdMeal() {
+        return idMeal;
+    }
+
+    public String getStrMeal() {
+        return strMeal;
+    }
+
+    public String getStrCategory() {
+        return strCategory;
+    }
+
+    public String getStrArea() {
+        return strArea;
+    }
+
+    public String getStrInstructions() {
+        return strInstructions;
+    }
+
+    public String getStrMealThumb() {
+        return strMealThumb;
+    }
+}
---
+++ app/src/main/java/hu/nje/recipefinder/data/remote/dto/MealResponse.java
@@ -0,0 +1,12 @@
+package hu.nje.recipefinder.data.remote.dto;
+
+import java.util.List;
+
+public class MealResponse {
+
+    private List<MealDto> meals;
+
+    public List<MealDto> getMeals() {
+        return meals;
+    }
+}
